67a0b1235fd399cea46435d13afd7724e21ffbe3
Ron <rumbletumjum@gmail.com>
2019-07-06 16:08:58 -0400

    render unto intellij that which is intellij's

--- src/org/ron/lox/Expr.java
+++ src/org/ron/lox/Expr.java
@@ -1,10 +1,9 @@
 package org.ron.lox;
 
-import java.util.List;
-import org.ron.lox.Token;
-
 abstract class Expr {
+
   static class Binary extends Expr {
+
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
@@ -17,6 +16,7 @@ abstract class Expr {
   }
 
   static class Grouping extends Expr {
+
     Grouping(Expr expression) {
       this.expression = expression;
     }
@@ -25,6 +25,7 @@ abstract class Expr {
   }
 
   static class Literal extends Expr {
+
     Literal(Object value) {
       this.value = value;
     }
@@ -33,6 +34,7 @@ abstract class Expr {
   }
 
   static class Unary extends Expr {
+
     Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
